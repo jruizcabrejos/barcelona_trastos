721657c8004f332e250a7b0c978874a4152f154a
ForgeGit <jruizcabrejos@gmail.com>
2026-04-05 20:26:45 +0200

    Update

--- 1_scrapper_2026.R
+++ 1_scrapper_2026.R
@@ -26,12 +26,12 @@ df <- read_csv("./indata/carrerer_BarOPENDATA.csv") %>%
 
 # Empty table to store the information
 df_temp <- data.frame(
-  
+
   i = as.integer(),
   df_name = as.character(),
   k = as.integer(),
   df_name_specific = as.character(),
-  
+
   Time = as.character(),
   CalendarURL = as.character(),
   Location = as.character(),
@@ -53,72 +53,72 @@ if(is.infinite(max(df_temp$i))){start_n <- 1} else {start_n <- max(df_temp$i)}
 ## Iterate through all the ~4,000 street names in Barcelona
 
 for(i in start_n:nrow(df)){
-  
+
   tryCatch({
-    
+
     ## Navigate to website
     remDr$navigate("https://ajuntament.barcelona.cat/cercador-de-residus/es")
-    Sys.sleep(1.5)
-    
+    Sys.sleep(2)
+
     ## Select Muebles and Trastos
     inputElement <- remDr$findElement(using = "id", value = "generic-search")
     inputElement$sendKeysToElement(list("Muebles y trastos"))
-    Sys.sleep(0.5)
-    
+    Sys.sleep(1)
+
     ## Select Street
     inputElement <- remDr$findElement(using = "id", value = "cerca")
     inputElement$sendKeysToElement(list(df$name[i]))
-    Sys.sleep(0.5)
+    Sys.sleep(1)
     inputElement$sendKeysToElement(list(key = "space"))
     Sys.sleep(2)
-    
+
     ## Check if street is valid
     tryCatch({
-      carrer_exists <- remDr$findElement(using = "xpath", 
+      carrer_exists <- remDr$findElement(using = "xpath",
                                          value = '//*[@id="err_cerca" and contains(text(), "Sin resultados")]')
     }, error = function(e) {NULL})
-    
+
     ## Only continue if street is valid
     if(!exists("carrer_exists")){
-      
+
       ## List all the street numbers available
       opts <- remDr$findElements(using = "css selector",
                                  value = "li.ui-menu-item a")
-      
+
       df_list <- data.frame(
         idx   = seq_along(opts),
         label = vapply(opts,
                        function(x) x$getElementText()[[1]],
                        character(1)
         ), stringsAsFactors = FALSE)
-      
+
       street_numbers <- as.numeric(df$nre_max[i]) - as.numeric(df$nre_min[i])
-      
+
       if(street_numbers - nrow(df_list) > 15){
-        k_total = street_numbers 
+        k_total = street_numbers
       } else {
         k_total = nrow(df_list)
       }
-      
+
       ## Iterate through SOME of the street numbers
       ## Here I check for every 5 (e.g. Carrer 1, Carrer 5, Carrer 10...)
       for(k in seq(0,k_total,5)){
-        
+
         if(nrow(df_list)>=1){ # Sanity check - already validated above
-          
-          if(exists("df_list") & k >= 25){rm(df_list)}
-          
+
+          if(exists("df_list") & k > 25){rm(df_list)}
+
           if(k==0){k<-1} # If k = 0, then make it 1.
-          
+
           ## Select Street
-          
+
           if(k>25){
-            
+
             inputElement$sendKeysToElement(list(as.character(k)))
             Sys.sleep(0.5)
             inputElement$sendKeysToElement(list(key = "space"))
             Sys.sleep(2)
-            
+
             ## List all the street numbers available
             opts <- remDr$findElements(using = "css selector",
                                        value = "li.ui-menu-item a")
@@ -128,17 +128,17 @@ for(i in start_n:nrow(df)){
                              function(x) x$getElementText()[[1]],
                              character(1)
               ), stringsAsFactors = FALSE)
-            
+
             ## Check if street is valid
             tryCatch({
-              carrer_exists_2 <- remDr$findElement(using = "xpath", 
+              carrer_exists_2 <- remDr$findElement(using = "xpath",
                                                    value = '//*[@id="err_cerca" and contains(text(), "Sin resultados")]')
             }, error = function(e) {NULL})
-            
+
             if(!exists("carrer_exists_2")){
               opts[[1]]$clickElement()
             }
-            
+
           } else {
             opts[[k]]$clickElement()
             Sys.sleep(0.5)
@@ -147,117 +147,121 @@ for(i in start_n:nrow(df)){
             Sys.sleep(0.5)
             opts[[k]]$clickElement()
           }
-          
+
           Sys.sleep(1)
-          
+
           if(!exists("carrer_exists_2")){
-            
+
             ## Scroll to "Confirm" button
             webElem <- remDr$findElement(using = "css selector", "a.submit.btn.btn-dark.do-search-residu")
             remDr$executeScript("arguments[0].scrollIntoView(true);", list(webElem))
-            
+
             ## Navigate to "Confirm" page
             href_value <- as.character(webElem$getElementAttribute("href"))
             remDr$navigate(paste0("https://ajuntament.barcelona.cat/cercador-de-residus/es/resid/RM0093/",
                                   last_numbers <- sub(".*/([^/]+)$", "\\1", href_value)))
             Sys.sleep(1.5)
-            
+
             tryCatch({
               v_dataexists <- try(remDr$findElement(using = "xpath", "//div[@class='info-altres quan porta-porta']//p[1]"))
               if(class(v_dataexists)[1] != "webElement"){
-                
+
                 target_xpath <- "//h3[contains(text(), 'Información adicional')]/following-sibling::p[3]"
                 v_dataexists <- remDr$findElements(using = "xpath", target_xpath)
                 location <- v_dataexists[[1]]$getElementText()[[1]]
-                
+                time <- v_dataexists[[1]]$getElementText()[[1]]
+
               } else if(class(v_dataexists)[1] == "webElement"){
-                
+
                 time <- remDr$findElement(using = "xpath", "//div[@class='info-altres quan porta-porta']//p[1]")$getElementText()
                 calendar_url <- remDr$findElement(using = "xpath", "//a[@class='link']")$getElementAttribute("href")
                 location <- remDr$findElement(using = "xpath", "//div[@class='info-altres quan porta-porta']//p[2]")$getElementText()
-                
+
               } else if(class(v_dataexists)[1] == "try-error"){
-                
+
                 time <- NA
                 calendar_url <- NA
                 location <- NA
               }
-              
-              
-            }, error = function(e) {     
-              
+
+
+            }, error = function(e) {
+
               time <- NA
               calendar_url <- NA
               location <- NA
-              
+
             }, warning = function(w) {
-              
+
               time <- NA
               calendar_url <- NA
               location <- NA
-              
+
             })
           }
-          
+
           if(exists("carrer_exists_2")){rm(carrer_exists_2)}
-          
+
           if(k>25){k_index<-1} else {k_index<-k}
-          
+
           ## Add data to table
           df_temp <- bind_rows(df_temp,
                                data.frame(
                                  i = as.integer(i),
                                  df_name = df$name[i],
-                                 
+
                                  k = as.integer(k),
                                  df_name_specific = df_list$label[k_index],
-                                 
+
                                  Time = time[[1]],
                                  CalendarURL = calendar_url[[1]],
                                  Location = location[[1]],
                                  stringsAsFactors = FALSE
                                )
           )
-          
+
           ##==##==## Return to Yojamba ##==##==##
           # Navigate back to the beginning.
           # Leaves you at the beginning of the loop.
-          
+
+          if(exists("time")){rm(time)}
+
           if(k < max(seq(0,k_total,5))){
             remDr$navigate("https://ajuntament.barcelona.cat/cercador-de-residus/es")
             Sys.sleep(2)
-            
+
             inputElement <- remDr$findElement(using = "id", value = "generic-search")
             inputElement$sendKeysToElement(list("Muebles y trastos"))
             #Sys.sleep(2)
-            
+
             inputElement <- remDr$findElement(using = "id", value = "cerca")
             inputElement$sendKeysToElement(list(df$name[i]))
-            
-            Sys.sleep(1) 
+
+            Sys.sleep(1)
             inputElement$sendKeysToElement(list(key = "space"))
             Sys.sleep(1)
-            
+
             opts <- remDr$findElements(using = "css selector",
                                        value = "li.ui-menu-item a")
           }
         }
       }
     }
-    
+
   }, error = function(e) {NULL})
-  
+
   if(exists("carrer_exists")){rm(carrer_exists)}
   if(exists("carrer_exists_2")){rm(carrer_exists_2)}
-  
-  if(i%%250 == 0){
-    
+  if(exists("time")){rm(time)}
+
+  if(i%%100 == 0){
+
     write.csv(df_temp,
               paste0("./df_temp_trashdata_",
                      format(Sys.Date(),"%Y%m%d"),
                      "_",i,".csv")
     )
-    
+
   }
 }
 
--- 3_maps_2026.R
+++ 3_maps_2026.R
@@ -20,6 +20,56 @@ library(scales)
 library(ggridges)
 library(ggpubr)
 library(magick)
+library(sf)
+
+theme_barcelona_trashmap <- function(scale_factor = 5) {
+  theme_void() +
+    theme(
+      ## Background Off-white/Cream
+      plot.background = element_rect(fill = "#FAF9F6", color = NA),
+      panel.background = element_rect(fill = "#FAF9F6", color = NA),
+
+      ## Using Montserrat for a "modern" feel
+      text = element_text(family = "montserrat"),
+
+      axis.ticks.x = element_blank(),
+      axis.text.x = element_blank(),
+      axis.ticks.y = element_blank(),
+      axis.text.y = element_blank(),
+      panel.grid = element_blank(),
+
+      plot.caption = ggtext::element_markdown(
+        hjust = 0, vjust = 1,
+        face = "italic", size = scale_factor * 7,
+        lineheight = 0.4,
+        family = "montserrat"
+      ),
+
+      plot.title = ggtext::element_markdown(
+        face = "bold", size = scale_factor * 18,
+        hjust = 0,
+        lineheight = 0.3,
+        family = "montserrat"
+      ),
+
+      plot.subtitle = ggtext::element_markdown(
+        face = "italic",
+        size = scale_factor * 14,
+        lineheight = 0.3,
+        family = "montserrat",
+        margin = margin(b = 10)
+      ),
+
+      legend.position = "inside",
+      legend.position.inside = c(.2, .28),
+      legend.title = element_text(size = scale_factor * 12, face = "bold"),
+      legend.text = element_text(size = scale_factor * 11, lineheight = 0.5),
+      legend.background = element_rect(fill = alpha('#FAF9F6', 0), color = NA),
+
+      plot.margin = margin(20, 20, 20, -20, "pt")
+    )
+}
+
 
 # ================================
 # ============  SETUP ============
@@ -27,9 +77,9 @@ library(magick)
 
 ## Export settings
 
-scale_factor = 5
+# scale_factor = 5
 # variable name      |n      or | or \ Symbol on Keyboard
-# "forge-"    
+# "forge-"
 # e901 github
 # e900 discord
 font_add(family = "emojis", "./indata/emojis.ttf")
@@ -37,22 +87,29 @@ font_import()
 n
 showtext_auto(TRUE)
 
+sysfonts::font_add_google("Montserrat", "montserrat")
+showtext::showtext_auto()
+
 ## Read shapefiles
 sf_AEB <- sf::st_read("./indata/Barcelona_shp/0301040100_AEB_UNITATS_ADM.shp")
 sf_barris <- sf::st_read("./indata/Barcelona_shp/0301040100_Barris_UNITATS_ADM.shp")
 sf_districts <- sf::st_read("./indata/Barcelona_shp/0301040100_Districtes_UNITATS_ADM.shp")
 sf_roads <- sf::st_read("./indata/BCN_GrafVial_SHP/BCN_GrafVial_Trams_ETRS89_SHP.shp")
 
+sf_cens <- sf::st_read("./indata/Barcelona_shp/0301040100_SecCens_UNITATS_ADM.shp")
+
 ## Read data
-df <- read_csv("./geocoded_scrapped_data20260401.csv") %>% 
-  
+df <- read_csv("./outdata/geocoded_scrapped_data20260401.csv") %>%
+
+  bind_rows(read_csv("./outdata/extra_geocoded_1.csv")) %>%
+  bind_rows(read_csv("./outdata/extra_geocoded_2.csv")) %>%
   ## Clean variables
   janitor::clean_names() %>%
   dplyr::select(-c(x1,x2)) %>%
-  
+
   ## Filter out "muebles y trastos viejos en el teléfono 010"
   filter(nchar(time) < 50) %>%
-  
+
   ## Encode variable of interst
   mutate(Time_custom = case_when(time=="Lunes, de 20.00 a 22.00 h."~ "Lunes",
                                  time=="Martes, de 20.00 a 22.00 h."~ "Martes",
@@ -65,7 +122,33 @@ df <- read_csv("./geocoded_scrapped_data20260401.csv") %>%
                                 "Miércoles","Jueves","Viernes"),
                               c("Lunes","Martes",
                                 "Miércoles","Jueves","Viernes"))) %>%
-  
+
+  ## Encode variable of interst
+  mutate(Time_custom_eng = case_when(time=="Lunes, de 20.00 a 22.00 h."~ "Monday",
+                                     time=="Martes, de 20.00 a 22.00 h."~ "Tuesday",
+                                     time=="Miércoles, de 20.00 a 22.00 h."~ "Wednesday",
+                                     time=="Jueves, de 20.00 a 22.00 h."~ "Thursday",
+                                     time=="Viernes, de 20.00 a 22.00 h."~ "Friday",
+                                     TRUE ~ time)) %>%
+  mutate(Time_custom_eng = factor(Time_custom_eng,
+                                  c("Monday","Tuesday",
+                                    "Wednesday","Thursday","Friday"),
+                                  c("Monday","Tuesday",
+                                    "Wednesday","Thursday","Friday"))) %>%
+
+  ## Encode variable of interst
+  mutate(Time_custom_cat = case_when(time == "Lunes, de 20.00 a 22.00 h."    ~ "Dilluns",
+                                     time == "Martes, de 20.00 a 22.00 h."   ~ "Dimarts",
+                                     time == "Miércoles, de 20.00 a 22.00 h." ~ "Dimecres",
+                                     time == "Jueves, de 20.00 a 22.00 h."  ~ "Dijous",
+                                     time == "Viernes, de 20.00 a 22.00 h."    ~ "Divendres",
+                                     TRUE ~ time)) %>%
+  mutate(Time_custom_cat = factor(Time_custom_cat,
+                                  c("Dilluns", "Dimarts",
+                                    "Dimecres", "Dijous", "Divendres"),
+                                  c("Dilluns", "Dimarts",
+                                    "Dimecres", "Dijous", "Divendres"))) %>%
+
   ## For good measure from previous version
   distinct(df_name,k,df_name_specific,.keep_all = T)
 
@@ -73,38 +156,79 @@ df <- read_csv("./geocoded_scrapped_data20260401.csv") %>%
 # ==================================
 # ============  PROCESS ============
 # ==================================
+#
+##==##==##==##
+##  By AEB  ##
+##==##==##==##
 
-sf_city <- st_as_sf(df %>% 
-                      filter(!is.na(lon)), 
-                    
-                    coords=c("lon","lat"),
-                    crs=4326, remove=F) %>% 
-  
+# Lat/Long Points are transformed into spatial objects
+sf_AEB_dots <- st_as_sf(df %>%
+                          filter(!is.na(lon)),
+
+                        coords=c("lon","lat"),
+                        crs=4326, remove=F) %>%
+
   st_transform(st_crs(sf_AEB)) %>%
+
+  # Lat/Long Points are classified depending on which AEB area they "fall into"
   mutate(area_class_1 = lengths(st_within(.,sf_AEB)))
 
-sf_final <- sf_city %>% 
-  
-  
+sf_AEB <- sf_AEB_dots %>%
+
   group_by(lat,lon) %>%
   slice(1) %>%
-  ungroup() %>% 
-  
-  
+  ungroup() %>%
+
+
   st_join(sf_AEB) %>%
   as.data.frame(.) %>%
-  
+
   ## Count by AEB
-  group_by(Time_custom, AEB) %>%
+  group_by(Time_custom,Time_custom_eng,Time_custom_cat, AEB) %>%
   tally() %>%
-  
+
   ## Filter by AEB
   group_by(AEB) %>%
-  filter(n==max(n))
-  
-  
-sf_AEB <- sf_AEB %>%
-  left_join(sf_final, by=c("AEB"))
+  filter(n==max(n)) %>%
+
+  left_join(sf_AEB, by=c("AEB"))
+
+
+##==##==##==##
+## By Censo ##
+##==##==##==##
+#
+# sf_censo <- st_as_sf(df %>%
+#                       filter(!is.na(lon)),
+#
+#                     coords=c("lon","lat"),
+#                     crs=4326, remove=F) %>%
+#
+#   st_transform(st_crs(sf_cens)) %>%
+#   mutate(area_class_1 = lengths(st_within(.,sf_cens)))
+#
+# sf_final_censo <- sf_censo %>%
+#
+#
+#   group_by(lat,lon) %>%
+#   slice(1) %>%
+#   ungroup() %>%
+#
+#
+#   st_join(sf_cens) %>%
+#   as.data.frame(.) %>%
+#
+#   ## Count by Census area and AEB
+#   group_by(Time_custom, AEB,SEC_CENS) %>%
+#   tally() %>%
+#
+#   ## Filter by Census area and AEB
+#   group_by(AEB,SEC_CENS) %>%
+#   filter(n==max(n))
+#
+#
+# sf_cens <- sf_cens %>%
+#   left_join(sf_final_censo, by=c("AEB","SEC_CENS"))
 
 # ===================================
 # ============  PLOTTING ============
@@ -113,150 +237,81 @@ sf_AEB <- sf_AEB %>%
 ## Others
 colors <- c("#D40000","#FFCC00","#D97D4B","#006747","#005F7F")
 
-
-# sf_city  %>%
-#   ggplot()+
-#   geom_sf(data=sf_city%>%
-#             filter(nchar(time) < 50 &
-#                      area_class_1==1), aes(color=time))+
-#   geom_sf(fill=NA)
-
-##==##==##==## 
+##==##==##==##
 ## Dot plot ##
-##==##==##==## 
+##==##==##==##
 plot_dots <- ggplot()+
-  
+
   ## Data layer
-  geom_sf(data=sf_city%>%
+  geom_sf(data=sf_AEB_dots%>%
             filter(area_class_1==1) %>% # Inside Barcelona
-            
             group_by(lat,lon) %>%
-            slice(1) %>%
-            ungroup(), 
-          
-          aes(color=Time_custom),alpha=0.35,stroke=NA,size=1)+
+            slice(1) %>% # Keep unique LAT/LON
+            ungroup(),
+
+          aes(color=Time_custom,geometry=geometry),
+          alpha=0.35,stroke=NA,size=1) +
+
   scale_color_manual(values = colors,
                      name="De 20.00h a 22.00h.")  +
-  
+
   ## Roads
   geom_sf(data=sf_roads,fill=NA,color=alpha("grey50"))+
   ## AEB
-  geom_sf(data=sf_AEB, fill = alpha("white",.25),color=alpha("black", .2), linetype = "dotted") +
+  geom_sf(data=sf_AEB,aes(geometry=geometry), fill = alpha("white",.25),color=alpha("black", .2), linetype = "dotted") +
   ## Barris
   geom_sf(data=sf_barris, fill=NA, color = alpha("black",.6), linetype = "dotted") +
   ## Districts
   geom_sf(data=sf_districts, fill=NA, color = alpha("black"))+
-  
+
   labs(title="Días de Recojo de Muebles y Trastos", subtitle="Siempre hay algo que encontrar.",
-       caption=c("<p><span style='font-family:emojis'>&#xe901;</span> /jruizcabrejos/barcelona_trastos   &#8212; Última Actualización:  &emsp; 29/01/2025 &#40;dd/mm/yyyy&#41;","<br>Fuente: https:&#47;&#47;www&#46;ajuntament.barcelona.cat/cercador-de-residus</p>"))+
-  
-  guides(color = guide_legend(override.aes = list(size=5,alpha=1))) +
-  
-  theme_bw()+
-  theme(axis.ticks.x = element_blank(),
-        axis.text.x = element_blank(),
-        axis.ticks.y = element_blank(),
-        axis.text.y = element_blank(),
-        
-        panel.grid = element_blank(),
-        panel.border = element_blank(),
-        plot.caption = element_markdown(hjust=0,vjust=1,
-                                        face="italic", size = scale_factor*9, 
-                                        lineheight = 0.4),
-        
-        plot.title = element_markdown(face = "bold", size = scale_factor * 17,
-                                      hjust = 0,
-                                      lineheight=0.3),
-        
-        plot.subtitle = element_markdown(face="italic",
-                                         size = scale_factor * 14,
-                                         lineheight=0.3),
-        legend.position = "inside",
-        legend.position.inside = c(.2, .28),   
-        # legend.spacing.x = unit(1, "pt"),
-        # legend.spacing.y = unit(0.5, "pt"),
-        # legend.direction="horizontal",
-        # legend.box.just = "left",
-        
-        legend.title = element_text(size=scale_factor*12),
-        legend.text = element_text(size = scale_factor * 11,
-                                   lineheight=0.5),
-        strip.text.x = element_text(size = scale_factor * 12),
-        legend.background = element_rect(fill = alpha('white', 0.4))#,
-        # plot.margin = margin(0, 0, 0, 0, "pt")
-  )
+       caption=c("<p><span style='font-family:emojis'>&#xe901;</span> /jruizcabrejos/barcelona_trastos",
+                 "<br>Última Actualización:  &emsp; 29/01/2025 &#40;dd/mm/yyyy&#41;",
+                 "<br><br>Fuente: https:&#47;&#47;www&#46;ajuntament.barcelona.cat/cercador-de-residus</p>"))+
+
+  guides(color = guide_legend(override.aes = list(size=5,alpha=1)))  +
+
+  theme_barcelona_trashmap()
 
 
-ggsave("./mapsV2/Barcelona_Basura_Muebles_Mapa_Dias_puntos_raw6_filter.png",plot_dots,
-       width=8,height=10,units="in",device="png",dpi=300)
+ggsave("./image/esp_Puntos_Mapa_Barcelona_Muebles_Trastos_Dias.png",plot_dots,
+       width=10,height=10,units="in",device="png",dpi=300)
 
 
-
-
-##==##==##==##==## 
+##==##==#==##==##
 ##  Filled plot ##
-##==##==##==##==## 
+##==##==##==##==##
 
 AEB_plot <- ggplot() +
-  
+
   ## Roads
   geom_sf(data=sf_roads,fill=NA, color=alpha("grey50"))+
-  
+
   ## Data layer
   geom_sf(data=sf_AEB, # Inside Barcelona
-          
-          aes(fill=Time_custom),alpha=0.7, size=0.5)+
+
+          aes(fill=Time_custom, geometry = geometry),alpha=0.7, size=0.5)+
   scale_fill_manual(values = colors,
-                     name="De 20.00h a 22.00h.")   +
+                     name="De 20.00h a 22.00h.",na.translate = F)   +
   ## AEB
-  geom_sf(data=sf_AEB, fill=NA, color=alpha("black", .2), linetype = "dotted") +
+  geom_sf(data=sf_AEB,aes(geometry = geometry), fill=NA, color=alpha("black", .2), linetype = "dotted") +
   ## Barris
-  geom_sf(data=sf_barris, fill=NA, color = alpha("black",.6), size=0.5) +
+  geom_sf(data=sf_barris, fill=NA, color = alpha("black",.6), linewidth =0.2) +
   ## Districts
-  geom_sf(data=sf_districts, fill=NA, color = alpha("black"), size=1)+
-  
+  geom_sf(data=sf_districts, fill=NA, color = alpha("black"),linewidth=0.8)+
+
   labs(title="Días de Recojo de Muebles y Trastos", subtitle="Siempre hay algo que encontrar.",
-       caption=c("<p><span style='font-family:emojis'>&#xe901;</span> /jruizcabrejos/barcelona_trastos   &#8212; Última Actualización:  &emsp; 29/01/2025 &#40;dd/mm/yyyy&#41;","<br>Fuente: https:&#47;&#47;www&#46;ajuntament.barcelona.cat/cercador-de-residus</p>"))+
-  
-  guides(color = guide_legend(override.aes = list(size=5,alpha=1))) +
-  
-  theme_bw()+
-  theme(axis.ticks.x = element_blank(),
-        axis.text.x = element_blank(),
-        axis.ticks.y = element_blank(),
-        axis.text.y = element_blank(),
-        
-        panel.grid = element_blank(),
-        panel.border = element_blank(),
-        plot.caption = element_markdown(hjust=0,vjust=1,
-                                        face="italic", size = scale_factor*9, 
-                                        lineheight = 0.4),
-        
-        plot.title = element_markdown(face = "bold", size = scale_factor * 17,
-                                      hjust = 0,
-                                      lineheight=0.3),
-        
-        plot.subtitle = element_markdown(face="italic",
-                                         size = scale_factor * 14,
-                                         lineheight=0.3),
-        legend.position = "inside",
-        legend.position.inside = c(.2, .28),   
-        # legend.spacing.x = unit(1, "pt"),
-        # legend.spacing.y = unit(0.5, "pt"),
-        # legend.direction="horizontal",
-        # legend.box.just = "left",
-        
-        legend.title = element_text(size=scale_factor*12),
-        legend.text = element_text(size = scale_factor * 11,
-                                   lineheight=0.5),
-        strip.text.x = element_text(size = scale_factor * 12),
-        legend.background = element_rect(fill = alpha('white', 0))#,
-        # plot.margin = margin(0, 0, 0, 0, "pt")
-  )
+       caption=c("<p><span style='font-family:emojis'>&#xe901;</span> /jruizcabrejos/barcelona_trastos",
+                 "<br>Última Actualización:  &emsp; 29/01/2025 &#40;dd/mm/yyyy&#41;",
+                 "<br><br>Fuente: https:&#47;&#47;www&#46;ajuntament.barcelona.cat/cercador-de-residus</p>"))+
 
-AEB_plot
-ggsave("./mapsV2/Barcelona_Basura_Muebles_Mapa_Dias_AEB_raw6_filter.png",AEB_plot,
-       width=8,height=10,units="in",device="png",dpi=300)
+  guides(color = guide_legend(override.aes = list(size=5,alpha=1)))  +
+
+  theme_barcelona_trashmap()
+
+ggsave("./image/esp_Mapa_Barcelona_Muebles_Trastos_Dias_TEST.png",AEB_plot,
+       width=10,height=10,units="in",device="png",dpi=300)
 
 
+source("./image/_maps_eng_cat.R")
 
